e599e056d0d6701a58d73da9919ad044be9ea7c8
rakib883 <sheikhrakib883@gmail.com>
2024-05-30 22:03:09 +0600

    commit

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,26 @@
-import NextAuth from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 import clientPromise from "@/lib/db";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
-
+import { Adapter } from "next-auth/adapters"; // Ensure this import matches your NextAuth version
 
 export interface Amajson {
   authOptions: AuthOptions;
 }
-const authOptions = {
+
+const authOptions: AuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
     }),
     GitHubProvider({
-      clientId: process.env.GITHUB_ID !,
-      clientSecret: process.env.GITHUB_SECRET !,
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
-  adapter:MongoDBAdapter(clientPromise)
+  adapter: MongoDBAdapter(clientPromise) as Adapter,
 };
 
 const handler = NextAuth(authOptions);
